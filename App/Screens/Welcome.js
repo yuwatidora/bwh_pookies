7c3a6a2752182d6d27399fb33dc95ec61ba97af3
Kelly Huang <khuan87@outlook.com>
2024-02-03 20:15:38 -0500

    Fixed errors and erased duplicated login

--- App/Screens/Welcome.js
+++ App/Screens/Welcome.js
@@ -10,7 +10,9 @@ import { ScrollView } from "react-native";
 export default function Login({ navigation }) {
   return (
     <ScrollView>
+    <ScrollView>
     <View className="flex-1 flex justify-around my-4">
+      
       
         <Image source={motherbaby} style={styles.appImage} />
         <View
@@ -23,6 +25,7 @@ export default function Login({ navigation }) {
             borderTopRightRadius: 20,
           }}
         >
+        
         
           <Text style={styles.heading}>The Postpartum Care</Text>
           <Text style={styles.heading}>You Need & Deserve.</Text>
@@ -137,7 +140,8 @@ export default function Login({ navigation }) {
                   </TouchableOpacity>
                 </View>
             </View>
-      </View>
+          </View>
+        </View>
     </View>
     </ScrollView>
   );
