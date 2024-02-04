ac9592e40ac42d411bf0b1d8ca5f4a0f1253332e
HninL <100245412+hninl23@users.noreply.github.com>
2024-02-04 00:30:35 -0500

    Revert "Log Out Button"

--- App/Screens/Welcome.js
+++ App/Screens/Welcome.js
@@ -11,6 +11,7 @@ export default function Login({ navigation }) {
   return (
     <ScrollView>
     <View className="flex-1 flex justify-around my-4">
+      
         <Image source={motherbaby} style={styles.appImage} />
         <View
           style={{
@@ -22,6 +23,7 @@ export default function Login({ navigation }) {
             borderTopRightRadius: 20,
           }}
         >
+        
           <Text style={styles.heading}>The Postpartum Care</Text>
           <Text style={styles.heading}>You Need & Deserve.</Text>
           <Text
@@ -47,11 +49,12 @@ export default function Login({ navigation }) {
                 marginTop: 20,
                 width: Dimensions.get("screen").width * 0.6,
               }}>
-              <Text className="text-xl font-bold text-center text-gray-700" style={{ fontSize: 19, color: Colors.fontColor, fontWeight: "bold" }}>
+              <Text
+                className="text-xl font-bold text-center text-gray-700" style={{ fontSize: 19, color: Colors.fontColor, fontWeight: "bold" }}
+              >
                 Log In
               </Text>
             </TouchableOpacity>
-            
             <View className="flex-row justify-center">
               <Text className="text-white font-semibold" style={{
                 textAlign: "center",
@@ -70,51 +73,10 @@ export default function Login({ navigation }) {
                   color: "orange"
                 }}> Sign Up</Text>
               </TouchableOpacity>
-              <Text>
-                Track, Care & Heal
-              </Text>
-              <View className="space-y-4">
-                <TouchableOpacity
-                  onPress={() => navigation.navigate('Login')}
-                  className="py-3 bg-yellow-400 mx-7 rounded-xl"
-                  style={{
-                    padding: 25,
-                    backgroundColor: Colors.pink,
-                    borderRadius: 120,
-                    alignItems: "center",
-                    marginTop: 20,
-                    width: Dimensions.get("screen").width * 0.6,
-                  }}>
-                  <Text
-                    className="text-xl font-bold text-center text-gray-700" style={{ fontSize: 19, color: Colors.fontColor, fontWeight: "bold" }}
-                  >
-                    Log In
-                  </Text>
-                </TouchableOpacity>
-                <View className="flex-row justify-center">
-                  <Text className="text-white font-semibold" style={{
-                    textAlign: "center",
-                    marginTop: 20,
-                    fontWeight: "600",
-                    color: Colors.fontColor,
-                    fontSize: 16
-                  }}>Don't have an account yet?</Text>
-                  <TouchableOpacity onPress={() => navigation.navigate('SignUp')}>
-                    <Text className="font-semibold text-yellow-400" style={{
-                      textAlign: "center",
-                      marginTop: 10,
-                      fontWeight: "600",
-                      color: Colors.fontColor,
-                      fontSize: 16,
-                      color: "orange"
-                    }}> Sign Up</Text>
-                  </TouchableOpacity>
-                </View>
-              </View>
             </View>
           </View>
-          </View>
-            </View>
+        </View>
+    </View>
     </ScrollView>
   );
 }
