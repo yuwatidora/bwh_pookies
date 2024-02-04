b0ae03c2c25ace0628c1bc191509137441da39bf
yuwatidora <nyi22y@mtholyoke.edu>
2024-02-03 23:23:07 -0500

    changes to existing files

--- App/navigation/appNavigation.js
+++ App/navigation/appNavigation.js
@@ -6,6 +6,8 @@ import Home from '../Screens/Home';
 import Welcome from '../Screens/Welcome';
 import Login from '../Screens/Login';
 import SignUp from '../Screens/SignUp';
+import WaterTracker from '../Screens/WaterTracker';
+import WaterCheck from '../Screens/WaterCheck';
 
 const Stack = createNativeStackNavigator();
 
@@ -13,11 +15,13 @@ const Stack = createNativeStackNavigator();
 export default function AppNavigation() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName='Welcome'>
+      <Stack.Navigator initialRouteName='WaterTracker'>
         <Stack.Screen name="Home" options={{headerShown: false}} component={Home} />
         <Stack.Screen name="Welcome" options={{headerShown: false}} component={Welcome} />
         <Stack.Screen name="Login" options={{headerShown: false}} component={Login} />
         <Stack.Screen name="SignUp" options={{headerShown: false}} component={SignUp} />
+        <Stack.Screen name ="WaterTracker" options={{headerShown: false}} component={WaterTracker}/>
+        <Stack.Screen name ="WaterCheck" options={{headerShown: false}} component={WaterCheck}/>
       </Stack.Navigator>
     </NavigationContainer>
   )
