3b02af86044eb8bfc9c0008bffec6d0791ae70f3
mafruhamanal <mafruhamanal1@gmail.com>
2024-02-03 19:05:03 -0500

    Login Layout Should Work

--- App.js
+++ App.js
@@ -1,13 +1,16 @@
 import { StatusBar } from "expo-status-bar";
 import { SafeAreaView, StyleSheet, Text, View } from "react-native";
 import Login from "./App/Screens/Welcome";
-import { ClerkProvider, SignedIn, SignedOut } from "@clerk/clerk-expo";
-import SignInWithOAuth from "./App/Components/SignInWithOAuth";
 import Home from "./App/Screens/Home";
 import AppNavigation from "./App/navigation/appNavigation";
+import UserProvider from "./App/Context/userContext";
 
 export default function App() {
-  return <AppNavigation />;
+  return (
+    <UserProvider>
+      <AppNavigation />
+    </UserProvider>
+  );
 }
 
 const styles = StyleSheet.create({
