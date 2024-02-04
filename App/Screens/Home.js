33568fb08d4ddb895bf6b14d1b865cd7783c8f91
hninl23 <h.lwin0440@gmail.com>
2024-02-03 19:34:20 -0500

    Navigation to Symptom Page

--- App/Screens/Home.js
+++ App/Screens/Home.js
@@ -4,41 +4,49 @@ import { SafeAreaView } from "react-native-safe-area-context";
 import { signOut } from "firebase/auth";
 import { auth } from "../../config/firebase";
 import { UserContext } from "../Context/userContext";
+import { useNavigation } from '@react-navigation/native';
 
 export default function Home() {
-  const { user } = useContext(UserContext);
-  const handleLogout = async () => {
-    await signOut(auth);
-  };
-  return (
-    <SafeAreaView className="flex-1 flex-row justify-center items-center">
-      <Text
-        style={{
-          textAlign: "center",
-          marginTop: 20,
-          fontWeight: "600",
-          color: "grey",
-          fontSize: 16,
-        }}
-      >
-        Home Page: {user.email}
-      </Text>
-      <TouchableOpacity
-        onPress={handleLogout}
-        className="p-1 bg-red-400 rounded-lg"
-      >
-        <Text
-          style={{
-            textAlign: "center",
-            marginTop: 20,
-            fontWeight: "600",
-            color: "grey",
-            fontSize: 16,
-          }}
-        >
-          Logout
-        </Text>
-      </TouchableOpacity>
-    </SafeAreaView>
-  );
+    const { user } = useContext(UserContext);
+    const navigation = useNavigation();
+    const handleLogout = async () => {
+        await signOut(auth);
+    };
+    return (
+        <SafeAreaView className="flex-1 flex-row justify-center items-center">
+            <Text
+                style={{
+                    textAlign: "center",
+                    marginTop: 20,
+                    fontWeight: "600",
+                    color: "grey",
+                    fontSize: 16,
+                }}
+            >
+                Home Page: {user.email}
+            </Text>
+            <TouchableOpacity
+                onPress={() => navigation.navigate('Symptoms')}>
+                <Text>
+                    Symptom Check
+                </Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+                onPress={handleLogout}
+                className="p-1 bg-red-400 rounded-lg"
+            >
+                <Text
+                    style={{
+                        textAlign: "center",
+                        marginTop: 20,
+                        fontWeight: "600",
+                        color: "grey",
+                        fontSize: 16,
+                    }}
+                >
+                    Logout
+                </Text>
+            </TouchableOpacity>
+        </SafeAreaView>
+    );
 }
