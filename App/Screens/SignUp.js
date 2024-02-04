b78d9ec1cd215cfd5b66b45bba211bc6cbe3ec55
hninl23 <h.lwin0440@gmail.com>
2024-02-04 10:02:29 -0500

    style for Sign Up

--- App/Screens/SignUp.js
+++ App/Screens/SignUp.js
@@ -5,7 +5,9 @@ import {
   Image,
   Dimensions,
   TextInput,
+  StyleSheet
 } from "react-native";
+import Colors from "../Shared/Colors";
 
 import { SafeAreaView } from "react-native-safe-area-context";
 import { ArrowLeftIcon } from "react-native-heroicons/solid";
@@ -18,179 +20,216 @@ import { UserContext } from "../Context/userContext";
 export default function SignUp() {
   const { name, setName } = useContext(UserContext);
   const navigation = useNavigation();
-  const [ email, setEmail ] = useState("");
-  const [ password, setPassword ] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-  const handleSubmit = async ()=>{
-    if(email && password){
-        try{
-            await createUserWithEmailAndPassword(auth, email, password);
+  const handleSubmit = async () => {
+    if (email && password) {
+      try {
+        await createUserWithEmailAndPassword(auth, email, password);
 
-        }catch(err){
-            console.log('got error: ',err.message);
-        }
+      } catch (err) {
+        console.log('got error: ', err.message);
+      }
     }
-}
+  }
   return (
-    <View style={{ backgroundColor: "white" }}>
-      <SafeAreaView className="flex ">
-        <View className="flex-row justify-start">
+    <View style={styles.whitePage}>
+      <SafeAreaView style={styles.Page}>
+        <View style={styles.Container}>
           <TouchableOpacity
             onPress={() => navigation.goBack()}
-            style={{
-              padding: 25,
-              backgroundColor: "pink",
-              borderRadius: 120,
-              alignItems: "center",
-              marginTop: 20,
-              width: Dimensions.get("screen").width * 0.6,
-            }}
+            style={styles.backArrow}
           >
             <ArrowLeftIcon size="20" color="black" />
           </TouchableOpacity>
         </View>
-        <View className="flex-row justify-center">
-          <Image
-            source={require("../../assets/images/signup.png")}
-            style={{ width: 200, height: 200 }}
-          />
+        <View style={styles.centerContainer}>
+          <View style={styles.ImageContainer}>
+            <Image
+              source={require("../../assets/images/signup.png")}
+              style={styles.image}
+            />
+          </View>
+
+          <View
+            style={styles.nameTitleBox}
+            className="flex-1 bg-white px-8 pt-8"
+          >
+            <View style={styles.inputField} className="form space-y-2">
+              <Text style={styles.nameText}>Name</Text>
+              <TextInput style={styles.nameTextInput} className="p-4 bg-gray-100 text-gray-700 rounded-2xl mb-3"
+                value={name}
+                onChangeText={(value) => setName(value)}
+                placeholder="Enter Name"
+              />
+              <Text style={styles.emailTitleBox}>
+                Email Address
+              </Text>
+              <TextInput style={styles.emailInput} placeholder="Enter Email" value={email}
+                onChangeText={(value) => setEmail(value)}
+              />
+              <Text style={styles.passwordTitle}>
+                Password
+              </Text>
+              <TextInput style={styles.passwordInput} secureTextEntry placeholder="Enter Password"
+                value={password}
+                onChangeText={(value) => setPassword(value)}
+              />
+              <TouchableOpacity onPress={handleSubmit} style={styles.SignUpBox}>
+                <Text style={styles.signUpText}>Sign Up</Text>
+              </TouchableOpacity>
+            </View>
+            <View style={styles.botContainer} className="flex-row justify-center space-x-12">
+              <TouchableOpacity style={styles.signupTextButton} className="p-2 bg-gray-100 rounded-2xl">
+              </TouchableOpacity>
+            </View>
+            <View className="flex-row justify-center mt-7">
+              <Text style={styles.accountText}>
+                Already have an account?
+              </Text>
+              <TouchableOpacity onPress={() => navigation.navigate("Login")}
+                style={styles.loginButton}>
+                <Text style={styles.LoginText}>Login</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
         </View>
       </SafeAreaView>
-      <View
-        style={{ borderTopLeftRadius: 50, borderTopRightRadius: 50 }}
-        className="flex-1 bg-white px-8 pt-8"
-      >
-        <View className="form space-y-2">
-        <Text style={{
-              textAlign: "center",
-              marginTop: 20,
-              fontWeight: "600",
-              color: "grey",
-              fontSize: 16,
-            }}>Name</Text>
-          <TextInput
-            style={{
-              textAlign: "center",
-              marginTop: 20,
-              fontWeight: "600",
-              color: "grey",
-              fontSize: 16,
-            }}
-            className="p-4 bg-gray-100 text-gray-700 rounded-2xl mb-3"
-            value={name}
-            onChangeText={(value) => setName(value)}
-            placeholder="Enter Name"
-          />
-          <Text
-            style={{
-              textAlign: "center",
-              marginTop: 20,
-              fontWeight: "600",
-              color: "grey",
-              fontSize: 16,
-            }}
-          >
-            Email Address
-          </Text>
-          <TextInput
-            style={{
-              textAlign: "center",
-              marginTop: 20,
-              fontWeight: "600",
-              color: "grey",
-              fontSize: 16,
-            }}
-            placeholder="email"
-            value={email}
-            onChangeText={(value) => setEmail(value)}
-          />
-          <Text
-            style={{
-              textAlign: "center",
-              marginTop: 20,
-              fontWeight: "600",
-              color: "grey",
-              fontSize: 16,
-            }}
-          >
-            Password
-          </Text>
-          <TextInput
-            style={{
-              textAlign: "center",
-              marginTop: 20,
-              fontWeight: "600",
-              color: "grey",
-              fontSize: 16,
-            }}
-            secureTextEntry
-            placeholder="Enter Password"
-            value={password}
-            onChangeText={(value) => setPassword(value)}
-          />
-          <TouchableOpacity
-            onPress={handleSubmit}
-            style={{
-              padding: 25,
-              backgroundColor: "pink",
-              borderRadius: 120,
-              alignItems: "center",
-              marginTop: 20,
-              width: Dimensions.get("screen").width * 0.6,
-            }}
-          >
-            <Text style={{
-              textAlign: "center",
-              marginTop: 10,
-              fontWeight: "600",
-              color: "grey",
-              fontSize: 20,
-            }}>Sign Up</Text>
-          </TouchableOpacity>
-        </View>
-        <View className="flex-row justify-center space-x-12">
-        <TouchableOpacity
-            style={{
-              padding: 10,
-              borderRadius: 120,
-              alignItems: "center",
-              marginTop: 20,
-              width: Dimensions.get("screen").width * 0.6,
-            }}
-            className="p-2 bg-gray-100 rounded-2xl"
-          >
-          </TouchableOpacity>
-          
-        </View>
-        <View className="flex-row justify-center mt-7">
-          <Text style={{
-              textAlign: "center",
-              marginTop: 20,
-              fontWeight: "600",
-              color: "grey",
-              fontSize: 16,
-            }}>
-            Already have an account?
-          </Text>
-          <TouchableOpacity 
-          onPress={() => navigation.navigate("Login")}
-          style={{
-            padding: 10,
-            borderRadius: 120,
-            alignItems: "center",
-            marginTop: 20,
-            width: Dimensions.get("screen").width * 0.6,
-          }}>
-            <Text style={{
-              textAlign: "center",
-              marginTop: 20,
-              fontWeight: "600",
-              color: "orange",
-              fontSize: 20,
-            }}>Login</Text>
-          </TouchableOpacity>
-        </View>
-      </View>
     </View>
+
   );
 }
+
+const styles = StyleSheet.create({
+  whitePage: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  Page: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  Image: {
+    width: 200,
+    height: 200
+  },
+  Container: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    paddingHorizontal: 20,
+    paddingTop: 20
+  },
+  centerContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  backArrow: {
+    alignItems: "center",
+    marginTop: "2%",
+    marginRight: "77%",
+    width: Dimensions.get("screen").width * 0.2,
+  },
+  loginButton: {
+    textAlign: "center",
+    margin: 0,
+    fontWeight: "600",
+    color: "grey",
+    fontSize: 16,
+  },
+  inputField: {
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  nameTitleBox: {
+    textAlign: "center",
+    marginTop: 20,
+    fontWeight: "600",
+    color: "grey",
+    fontSize: 16,
+  },
+  nameText: {
+    textAlign: "center",
+    marginTop: 20,
+    fontWeight: "500",
+    color: "#403939",
+    fontSize: 16,
+  },
+  nameTextInput: {
+    textAlign: "center",
+    marginTop: 20,
+    fontWeight: "600",
+    color: "grey",
+    fontSize: 16,
+  },
+  emailTitleBox: {
+    textAlign: "center",
+    marginTop: 20,
+    fontWeight: "500",
+    color: "#403939",
+    fontSize: 16,
+  },
+  emailInput: {
+    textAlign: "center",
+    marginTop: 20,
+    fontWeight: "600",
+    color: "grey",
+    fontSize: 16,
+  },
+  passwordTitle: {
+    textAlign: "center",
+    marginTop: 20,
+    fontWeight: "500",
+    color: "#403939",
+    fontSize: 16,
+  },
+  passwordInput: {
+    textAlign: "center",
+    marginTop: 20,
+    fontWeight: "600",
+    color: "grey",
+    fontSize: 16,
+  },
+  SignUpBox: {
+    padding: 10,
+    backgroundColor: Colors.pink,
+    borderRadius: 120,
+    alignContent: "center",
+    marginTop: 20,
+    textAlign: "center",
+    width: Dimensions.get("screen").width * 0.5,
+  },
+  signUpText: {
+    textAlign: "center",
+    fontWeight: "600",
+    color: Colors.fontColor,
+    fontSize: 16,
+    padding: "3%"
+  },
+  signupTextButton: {
+    padding: 10,
+    borderRadius: 120,
+    alignItems: "center",
+    width: Dimensions.get("screen").width * 0.6,
+  },
+  accountText: {
+    textAlign: "center",
+    fontWeight: "600",
+    color: "grey",
+    fontSize: 16,
+  },
+  LoginText: {
+    textAlign: "center",
+    fontWeight: "600",
+    color: "orange",
+    fontSize: 16,
+  },
+  botContainer: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center"
+  }
+})
